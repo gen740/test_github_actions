f1451724463b3595313350d8745716a8e4f90534
gen740 <54583542+gen740@users.noreply.github.com>
2026-01-09 16:50:07 +0900

    test(test): Redirect test output to stderr

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from pytest import mark
+import sys
 from tempfile_pool import NamedTemporaryFilePool
 
 logfile = "log.txt"
@@ -7,6 +8,6 @@ logfile = "log.txt"
 @mark.parametrize("i", range(10000))
 def test_case(i):
     with NamedTemporaryFilePool() as pool:
-        print(f"Test case {i} started {pool.name}")
+        print(f"Test case {i} started {pool.name}", file=sys.stderr)
         with open(logfile, "a") as f:
             f.write(f"Test case {i} started {pool.name}\n")
